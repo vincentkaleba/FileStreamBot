d5c98dd0e0c6ac23d6d90df085295e8c2c117efd
Hyosh code <153867266+vincentkaleba@users.noreply.github.com>
2025-09-12 07:27:56 +0200

    Update callback.py

--- FileStream/bot/plugins/callback.py
+++ FileStream/bot/plugins/callback.py
@@ -44,16 +44,16 @@ async def cb_data(bot, update: CallbackQuery):
         await update.message.delete()
     elif usr_cmd[0] == "msgdelete":
         await update.message.edit_caption(
-        caption= "**Cᴏɴғɪʀᴍ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴇʟᴇᴛᴇ ᴛʜᴇ Fɪʟᴇ**\n\n",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ʏᴇs", callback_data=f"msgdelyes_{usr_cmd[1]}_{usr_cmd[2]}"), InlineKeyboardButton("ɴᴏ", callback_data=f"myfile_{usr_cmd[1]}_{usr_cmd[2]}")]])
+        caption= "**Cᴏɴғɪʀᴍᴇᴢ ǫᴜᴇ ᴠᴏᴜs ᴠᴏᴜʟᴇᴢ sᴜᴘᴘʀɪᴍᴇʀ ʟᴇ ғɪᴄʜɪᴇʀ**\n\n",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴏᴜɪ", callback_data=f"msgdelyes_{usr_cmd[1]}_{usr_cmd[2]}"), InlineKeyboardButton("ɴᴏɴ", callback_data=f"myfile_{usr_cmd[1]}_{usr_cmd[2]}")]])
     )
     elif usr_cmd[0] == "msgdelyes":
         await delete_user_file(usr_cmd[1], int(usr_cmd[2]), update)
         return
     elif usr_cmd[0] == "msgdelpvt":
         await update.message.edit_caption(
-        caption= "**Cᴏɴғɪʀᴍ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴇʟᴇᴛᴇ ᴛʜᴇ Fɪʟᴇ**\n\n",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ʏᴇs", callback_data=f"msgdelpvtyes_{usr_cmd[1]}"), InlineKeyboardButton("ɴᴏ", callback_data=f"mainstream_{usr_cmd[1]}")]])
+        caption= "**Cᴏɴғɪʀᴍᴇᴢ ǫᴜᴇ ᴠᴏᴜs ᴠᴏᴜʟᴇᴢ sᴜᴘᴘʀɪᴍᴇʀ ʟᴇ ғɪᴄʜɪᴇʀ**\n\n",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴏᴜɪ", callback_data=f"msgdelpvtyes_{usr_cmd[1]}"), InlineKeyboardButton("ɴᴏɴ", callback_data=f"mainstream_{usr_cmd[1]}")]])
     )
     elif usr_cmd[0] == "msgdelpvtyes":
         await delete_user_filex(usr_cmd[1], update)
@@ -72,7 +72,7 @@ async def cb_data(bot, update: CallbackQuery):
     elif usr_cmd[0] == "userfiles":
         file_list, total_files = await gen_file_list_button(int(usr_cmd[1]), update.from_user.id)
         await update.message.edit_caption(
-            caption="Total files: {}".format(total_files),
+            caption="Tᴏᴛᴀʟ ᴅᴇ ғɪᴄʜɪᴇʀs : {}".format(total_files),
             reply_markup=InlineKeyboardMarkup(file_list)
             )
     elif usr_cmd[0] == "myfile":
@@ -81,7 +81,7 @@ async def cb_data(bot, update: CallbackQuery):
     elif usr_cmd[0] == "sendfile":
         myfile = await db.get_file(usr_cmd[1])
         file_name = myfile['file_name']
-        await update.answer(f"Sending File {file_name}")
+        await update.answer(f"Eɴᴠᴏɪ ᴅᴜ ғɪᴄʜɪᴇʀ {file_name}")
         await update.message.reply_cached_media(myfile['file_id'], caption=f'**{file_name}**')
     else:
         await update.message.delete()
@@ -106,52 +106,52 @@ async def gen_file_list_button(file_list_no: int, user_id: int):
         )
     if not file_list:
         file_list.append(
-                [InlineKeyboardButton("ᴇᴍᴘᴛʏ", callback_data="N/A")])
-    file_list.append([InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")])
+                [InlineKeyboardButton("ᴠɪᴅᴇ", callback_data="N/A")])
+    file_list.append([InlineKeyboardButton("ғᴇʀᴍᴇʀ", callback_data="close")])
     return file_list, total_files
 
 async def gen_file_menu(_id, file_list_no, update: CallbackQuery):
     try:
         myfile_info=await db.get_file(_id)
     except FIleNotFound:
-        await update.answer("File Not Found")
+        await update.answer("Fɪᴄʜɪᴇʀ ɴᴏɴ ᴛʀᴏᴜᴠᴇ́")
         return
 
     file_id=FileId.decode(myfile_info['file_id'])
 
     if file_id.file_type in PHOTO_TYPES:
-        file_type = "Image"
+        file_type = "Iᴍᴀɢᴇ"
     elif file_id.file_type == FileType.VOICE:
-        file_type = "Voice"
+        file_type = "Vᴏɪx"
     elif file_id.file_type in (FileType.VIDEO, FileType.ANIMATION, FileType.VIDEO_NOTE):
-        file_type = "Video"
+        file_type = "Vɪᴅᴇ́ᴏ"
     elif file_id.file_type == FileType.DOCUMENT:
-        file_type = "Document"
+        file_type = "Dᴏᴄᴜᴍᴇɴᴛ"
     elif file_id.file_type == FileType.STICKER:
-        file_type = "Sticker"
+        file_type = "Aᴜᴛᴏᴄᴏʟʟᴀɴᴛ"
     elif file_id.file_type == FileType.AUDIO:
-        file_type = "Audio"
+        file_type = "Aᴜᴅɪᴏ"
     else:
-        file_type = "Unknown"
+        file_type = "Iɴᴄᴏɴɴᴜ"
 
     page_link = f"{Server.URL}watch/{myfile_info['_id']}"
     stream_link = f"{Server.URL}dl/{myfile_info['_id']}"
     if "video" in file_type.lower():
         MYFILES_BUTTONS = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("sᴛʀᴇᴀᴍ", url=page_link), InlineKeyboardButton("ᴅᴏᴡɴʟᴏᴀᴅ", url=stream_link)],
-                [InlineKeyboardButton("ɢᴇᴛ ғɪʟᴇ", callback_data=f"sendfile_{myfile_info['_id']}"),
-                 InlineKeyboardButton("ʀᴇᴠᴏᴋᴇ ғɪʟᴇ", callback_data=f"msgdelete_{myfile_info['_id']}_{file_list_no}")],
-                [InlineKeyboardButton("ʙᴀᴄᴋ", callback_data="userfiles_{}".format(file_list_no))]
+                [InlineKeyboardButton("ᴅɪғғᴜsɪᴏɴ", url=page_link), InlineKeyboardButton("ᴛᴇ́ʟᴇ́ᴄʜᴀʀɢᴇʀ", url=stream_link)],
+                [InlineKeyboardButton("ᴏʙᴛᴇɴɪʀ ʟᴇ ғɪᴄʜɪᴇʀ", callback_data=f"sendfile_{myfile_info['_id']}"),
+                 InlineKeyboardButton("ʀᴇᴠᴏǫᴜᴇʀ ʟᴇ ғɪᴄʜɪᴇʀ", callback_data=f"msgdelete_{myfile_info['_id']}_{file_list_no}")],
+                [InlineKeyboardButton("ʀᴇᴛᴏᴜʀ", callback_data="userfiles_{}".format(file_list_no))]
             ]
         )
     else:
         MYFILES_BUTTONS = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("ᴅᴏᴡɴʟᴏᴀᴅ", url=stream_link)],
-                [InlineKeyboardButton("ɢᴇᴛ ғɪʟᴇ", callback_data=f"sendfile_{myfile_info['_id']}"),
-                 InlineKeyboardButton("ʀᴇᴠᴏᴋᴇ ғɪʟᴇ", callback_data=f"msgdelete_{myfile_info['_id']}_{file_list_no}")],
-                [InlineKeyboardButton("ʙᴀᴄᴋ", callback_data="userfiles_{}".format(file_list_no))]
+                [InlineKeyboardButton("ᴛᴇ́ʟᴇ́ᴄʜᴀʀɢᴇʀ", url=stream_link)],
+                [InlineKeyboardButton("ᴏʙᴛᴇɴɪʀ ʟᴇ ғɪᴄʜɪᴇʀ", callback_data=f"sendfile_{myfile_info['_id']}"),
+                 InlineKeyboardButton("ʀᴇᴠᴏǫᴜᴇʀ ʟᴇ ғɪᴄʜɪᴇʀ", callback_data=f"msgdelete_{myfile_info['_id']}_{file_list_no}")],
+                [InlineKeyboardButton("ʀᴇᴛᴏᴜʀ", callback_data="userfiles_{}".format(file_list_no))]
             ]
         )
 
@@ -159,7 +159,7 @@ async def gen_file_menu(_id, file_list_no, update: CallbackQuery):
     if type(TiMe) == float:
         date = datetime.datetime.fromtimestamp(TiMe)
     await update.edit_message_caption(
-        caption="**File Name :** `{}`\n**File Size :** `{}`\n**File Type :** `{}`\n**Created On :** `{}`".format(myfile_info['file_name'],
+        caption="**ɴᴏᴍ ᴅᴜ ғɪᴄʜɪᴇʀ :** `{}`\n**ᴛᴀɪʟʟᴇ ᴅᴜ ғɪᴄʜɪᴇʀ :** `{}`\n**ᴛʏᴘᴇ ᴅᴇ ғɪᴄʜɪᴇʀ :** `{}`\n**ᴄʀᴇ́ᴇ́ ʟᴇ :** `{}`".format(myfile_info['file_name'],
                                                                                                                     humanbytes(int(myfile_info['file_size'])),
                                                                                                                     file_type,
                                                                                                                     TiMe if isinstance(TiMe,str) else date.date()),
@@ -171,14 +171,14 @@ async def delete_user_file(_id, file_list_no: int, update:CallbackQuery):
     try:
         myfile_info=await db.get_file(_id)
     except FIleNotFound:
-        await update.answer("File Already Deleted")
+        await update.answer("Fɪᴄʜɪᴇʀ ᴅᴇ́ᴊᴀ̀ sᴜᴘᴘʀɪᴍᴇ́")
         return
 
     await db.delete_one_file(myfile_info['_id'])
     await db.count_links(update.from_user.id, "-")
     await update.message.edit_caption(
-            caption= "**Fɪʟᴇ Dᴇʟᴇᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ !**" + update.message.caption.replace("Cᴏɴғɪʀᴍ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴇʟᴇᴛᴇ ᴛʜᴇ Fɪʟᴇ", ""),
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ʙᴀᴄᴋ", callback_data=f"userfiles_1")]])
+            caption= "**Fɪᴄʜɪᴇʀ sᴜᴘᴘʀɪᴍᴇ́ ᴀᴠᴇᴄ sᴜᴄᴄᴇ̀s !**" + update.message.caption.replace("Cᴏɴғɪʀᴍᴇᴢ ǫᴜᴇ ᴠᴏᴜs ᴠᴏᴜʟᴇᴢ sᴜᴘᴘʀɪᴍᴇʀ ʟᴇ ғɪᴄʜɪᴇʀ", ""),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ʀᴇᴛᴏᴜʀ", callback_data=f"userfiles_1")]])
         )
 
 async def delete_user_filex(_id, update:CallbackQuery):
@@ -186,13 +186,12 @@ async def delete_user_filex(_id, update:CallbackQuery):
     try:
         myfile_info=await db.get_file(_id)
     except FIleNotFound:
-        await update.answer("File Already Deleted")
+        await update.answer("Fɪᴄʜɪᴇʀ ᴅᴇ́ᴊᴀ̀ sᴜᴘᴘʀɪᴍᴇ́")
         return
 
     await db.delete_one_file(myfile_info['_id'])
     await db.count_links(update.from_user.id, "-")
     await update.message.edit_caption(
-            caption= "**Fɪʟᴇ Dᴇʟᴇᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ !**\n\n",
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data=f"close")]])
+            caption= "**Fɪᴄʜɪᴇʀ sᴜᴘᴘʀɪᴍᴇ́ ᴀᴠᴇᴄ sᴜᴄᴄᴇ̀s !**\n\n",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ғᴇʀᴍᴇʀ", callback_data=f"close")]])
         )
-
